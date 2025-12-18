5f676382359a37ebf7f5b5643f867676f99be39a
Jack Rich <j.b.c.rich@pgr.reading.ac.uk>
2025-12-18 02:10:49 +0000

    additions

--- src/gwel/cli.py
+++ src/gwel/cli.py
@@ -123,8 +123,6 @@ def detect(model: str, weights: str, slice_size:int = typer.Option(
                     detector = YOLOv8(weights)
                 else:
                     detector = YOLOv8(weights,patch_size=(slice_size,slice_size))
-                if cuda:
-                    detector.set_device('cuda')
             else:
                 raise ValueError("Model type unknown.")
         else:
