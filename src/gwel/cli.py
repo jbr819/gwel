943bff5e4ee13ac65378a568c9b66e84c7a362e5
Jack Rich <jackrich88@googlemail.com>
2026-07-08 19:52:18 +0100

    confidence threshold

--- src/gwel/cli.py
+++ src/gwel/cli.py
@@ -141,7 +141,7 @@ def resize(max_pixels: int = typer.Option(
 def detect(model: str = typer.Argument(...,help="Model type [Supported: YOLO, YOLOv8seg] "), 
            weights: str = typer.Argument(...,help="Path to model weights"), 
            slice_size:int = typer.Option(None, "--slicesz", "-s", help="Slice size"),
-           confidence:float = typer.Option(0.25, "--confidence", "-c", help="Confidence threshold [Default: 0.25]."),
+           confidence:float = typer.Option(0.25, "--confidence", "-c", help="Confidence threshold."),
            resized_flag: bool = typer.Option(False, "--resized", "-r", help="Detect objects in resized images."),
            add: bool = typer.Option(False,"--add","-a", help="Add detections to current detections. [Default: False]"),
            bbox_only:bool = typer.Option(False, "--bbox", "-b", help="Bounding boxes only. [Default: False]")):
